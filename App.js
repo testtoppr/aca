b0730fb7d5a8c822a156f4b4db850b4d620f3590
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-12-10 16:03:27 +0530

    done

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import SnackBar from './src/SnackBar'
+import { Audio } from 'expo-av';
 
 export default function App() {
   const [snackBarVisible, setSnackBarVisible] = useState(false);
@@ -19,8 +20,20 @@ export default function App() {
       setSnackBarVisible(false);
   }
 
-  function handeClick(){
-    displaySnackBar("success","Hello testing");
+  async function handeClick(){
+    displaySnackBar("success","This is an example of a snackbar");
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('./assets/ding.mp3'));
+      await soundObject.playAsync();
+      // Your sound is playing!
+
+      // Don't forget to unload the sound from memory
+      // when you are done using the Sound object
+      await soundObject.unloadAsync();
+    } catch (error) {
+      // An error occurred!
+    }
   }
   return (
     <View style={styles.container}>
